534015aa7838e4abe54e222b5d1a1bdbdb9e7172
Elano Garcez <elanogarcez@gmail.com>
2015-11-26 11:06:46 -0300

    correcao register controller e set ruby version in gemfile

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ group :development, :test  do
 end
 
 gem 'rails_12factor', group: :production
+
+ruby "2.2.3p173" 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nome :email, :password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nome, :email, :password) }
     end
 end
